2be772bcbc8ff17e19beb2b5713bd0a86efc30f5
Valentina Florez <valenflorca@gmail.com>
2024-01-24 22:14:33 -0500

    add paymont method page and page link

---
+++ darling.js
@@ -0,0 +1,113 @@
+productos: [
+ {
+    'id' : 1,
+    'nombre' : 'Luxury Gems Necklace',
+    'codigo' : '',
+    'precio_unitario' : '168.76',
+    'tipo_de_accesorio' : 'collar',
+    'imagen' :['assets\products\coleccion1.png'],
+    'descripcion' : 'collar de plata con diamante moissanite',
+    'stock_por_color': {'plata': 50, 'dorado': 20},
+ },
+
+ {
+    'id' : 2,
+    'nombre' : 'Aurora Ring',
+    'codigo' : '',
+    'precio_unitario' : '125.28',
+    'tipo_de_accesorio' : 'gargantilla',
+    'imagen' :['assets\products\coleccion2.png'],
+    'descripcion' : 'gargantilla en oro laminado con mini cristales',
+    'stock_por_color': {'plata': 20, 'oro': 32},
+ },
+
+ {
+    'id' : 3,
+    'nombre' : 'Reflections Necklace',
+    'codigo' : '',
+    'precio_unitario' : '620.73',
+    'tipo_de_accesorio' : 'brazalete',
+    'imagen' :['assets\products\coleccion3.png'],
+    'descripcion' : 'brazalete tejido en hilo de oro',
+    'stock_por_color': {'plata': 120, 'oro': 32},
+ },
+
+ {
+    'id' : 4,
+    'nombre' : 'Dreamy Infinity Ring',
+    'codigo' : '',
+    'precio_unitario' : '327.71',
+    'tipo_de_accesorio' : 'anillo',
+    'imagen' :['assets\products\coleccion4.png'],
+    'descripcion' : 'anillo con piedra amethist y mini cristales',
+    'stock_por_color': {'rosa': 120, 'plata': 41, 'dorado': 32},
+ },
+
+ {
+    'id' : 5,
+    'nombre' : 'Opulent Jewels Ring',
+    'codigo' : '',
+    'precio_unitario' : '168.76',
+    'tipo_de_accesorio' : 'anillo',
+    'imagen' :['assets\products\coleccion5.png'],
+    'descripcion' : 'anillo en oro blanco y oro dorado',
+    'stock_por_color': {'plateado-dorado': 120},
+ },
+
+ {
+    'id' : 6,
+    'nombre' : 'Serene Solitaire Earrings',
+    'codigo' : '',
+    'precio_unitario' : '125.28',
+    'tipo_de_accesorio' : 'aretes',
+    'imagen' :['assets\products\coleccion6.png'],
+    'descripcion' : 'aretes con piedra azurite',
+    'stock_por_color': {'azul': 55, 'aguamarina': 66},
+ },
+
+ {
+    'id' : 7,
+    'nombre' : 'Timeless Halo Earrings',
+    'codigo' : '',
+    'precio_unitario' : '620.73',
+    'tipo_de_accesorio' : 'aretes',
+    'imagen' :['assets\products\coleccion7.png'],
+    'descripcion' : 'aretes con cristal',
+    'stock_por_color': {'rosa': 22, 'plateado': 65, 'azul': 55, 'aguamarina': 66},
+ },
+
+ {
+    'id' : 8,
+    'nombre' : 'Exquisite Earrings',
+    'codigo' : '',
+    'precio_unitario' : '327.71',
+    'tipo_de_accesorio' : 'aretes',
+    'imagen' :['assets\products\coleccion8.png'],
+    'descripcion' : 'aretes con cristal en gota',
+    'stock_por_color': {'rosa': 22, 'plateado': 65, 'azul': 55, 'aguamarina': 66},
+ },
+
+ {
+    'id' : 9,
+    'nombre' : 'Timeless Elegance Ring',
+    'codigo' : '',
+    'precio_unitario' : '168.76',
+    'tipo_de_accesorio' : 'anillo',
+    'imagen' :['assets\products\prin-collection1.txt'],
+    'descripcion' : 'trio de anillos con piedras preciosas',
+    'stock_por_color': {'rosa': 22, 'morado': 53, 'azul': 55},
+ },
+
+ {
+    'id' : 10,
+    'nombre' : 'Luxury Charms Ring',
+    'codigo' : '',
+    'precio_unitario' : '620.73',
+    'tipo_de_accesorio' : 'anillo',
+    'imagen' :['assets\products\prin-collection1.txt'],
+    'descripcion' : 'trio de anillos con piedras preciosas',
+    'stock_por_color': {'rosa': 22, 'morado': 53, 'azul': 55},
+ }
+ 
+
+]
