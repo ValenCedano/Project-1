a172f010e2ebc2676f8e153d12b7c63d642eba52
Valentina Florez <valenflorca@gmail.com>
2024-01-29 21:19:54 -0500

    js link and lastest file changes

--- darling.js
+++ darling.js
@@ -1,3 +1,8 @@
+// Entrega Sprint #3
+// • Declarar una lista de los productos con los siguientes datos: id, nombre, código, precio
+// unitario, tipo de accesorio (anillo, brazalete, collar, aretes, etc.), imágenes, descripción,
+// cantidad en stock por color y/o talla.
+
 const productos = [
    {
       'id' : 1,
@@ -7,7 +12,11 @@ const productos = [
       'tipo_de_accesorio' : 'collar',
       'imagen' :['assets\products\coleccion1.png'],
       'descripcion' : 'collar de plata con diamante moissanite',
-      'stock_por_color': {'plata': 50, 'dorado': 20}
+      stock: [
+         { color: 'plata', talla: 6, cantidad: 23 },
+         { color: 'plata', talla: 7, cantidad: 18 },
+         { color: 'plata', talla: 8, cantidad: 44 }
+     ]
    },
    {
       'id' : 2,
@@ -17,7 +26,11 @@ const productos = [
       'tipo_de_accesorio' : 'gargantilla',
       'imagen' :['assets\products\coleccion2.png'],
       'descripcion' : 'gargantilla en oro laminado con mini cristales',
-      'stock_por_color': {'plata': 20, 'oro': 32},
+      stock: [
+         { color: 'plata', cantidad: 54 },
+         { color: 'oro', cantidad: 43 },
+         { color: 'acero', cantidad: 20 }
+     ]
    },
    
    {
@@ -27,8 +40,12 @@ const productos = [
       'precio_unitario' : '620.73',
       'tipo_de_accesorio' : 'brazalete',
       'imagen' :['assets\products\coleccion3.png'],
-      'descripcion' : 'brazalete tejido en hilo de oro',
-      'stock_por_color': {'plata': 120, 'oro': 32},
+      'descripcion' : 'brazalete tejido en hilo',
+      stock: [
+         { color: 'plata', cantidad: 16 },
+         { color: 'oro', cantidad: 24 },
+         { color: 'acero', cantidad: 31 }
+     ]
    },
    
    {
@@ -39,7 +56,15 @@ const productos = [
       'tipo_de_accesorio' : 'anillo',
       'imagen' :['assets\products\coleccion4.png'],
       'descripcion' : 'anillo con piedra amethist y mini cristales',
-      'stock_por_color': {'rosa': 120, 'plata': 41, 'dorado': 32},
+      stock: [
+         { color: 'plata', talla: 6, cantidad: 23 },
+         { color: 'plata', talla: 7, cantidad: 18 },
+         { color: 'plata', talla: 8, cantidad: 44 },
+         { color: 'oro', talla: 6, cantidad: 43 },
+         { color: 'oro', talla: 7, cantidad: 25 },
+         { color: 'oro', talla: 8, cantidad: 12 }
+
+     ]
    },
    
    {
@@ -50,7 +75,15 @@ const productos = [
       'tipo_de_accesorio' : 'anillo',
       'imagen' :['assets\products\coleccion5.png'],
       'descripcion' : 'anillo en oro blanco y oro dorado',
-      'stock_por_color': {'plateado-dorado': 120},
+      stock: [
+         { color: 'plata', talla: 6, cantidad: 19 },
+         { color: 'plata', talla: 7, cantidad: 21 },
+         { color: 'plata', talla: 8, cantidad: 45 },
+         { color: 'oro', talla: 6, cantidad: 22 },
+         { color: 'oro', talla: 7, cantidad: 18 },
+         { color: 'oro', talla: 8, cantidad: 19 }
+
+     ]
    },
    
     {
@@ -61,7 +94,11 @@ const productos = [
        'tipo_de_accesorio' : 'aretes',
        'imagen' :['assets\products\coleccion6.png'],
        'descripcion' : 'aretes con piedra azurite',
-       'stock_por_color': {'azul': 55, 'aguamarina': 66},
+       stock: [
+         { color: 'plata', cantidad: 29 },
+         { color: 'oro', cantidad: 17 },
+         { color: 'acero', cantidad: 33 }
+     ]
     },
    
     {
@@ -72,7 +109,11 @@ const productos = [
        'tipo_de_accesorio' : 'aretes',
        'imagen' :['assets\products\coleccion7.png'],
        'descripcion' : 'aretes con cristal',
-       'stock_por_color': {'rosa': 22, 'plateado': 65, 'azul': 55, 'aguamarina': 66},
+       stock: [
+         { color: 'plata', cantidad: 27 },
+         { color: 'oro', cantidad: 34 },
+         { color: 'acero', cantidad: 18 }
+     ]
     },
    
     {
@@ -83,7 +124,11 @@ const productos = [
        'tipo_de_accesorio' : 'aretes',
        'imagen' :['assets\products\coleccion8.png'],
        'descripcion' : 'aretes con cristal en gota',
-       'stock_por_color': {'rosa': 22, 'plateado': 65, 'azul': 55, 'aguamarina': 66},
+       stock: [
+         { color: 'plata', cantidad: 27 },
+         { color: 'oro', cantidad: 34 },
+         { color: 'acero', cantidad: 18 }
+     ]
     },
    
     {
@@ -94,7 +139,15 @@ const productos = [
        'tipo_de_accesorio' : 'anillo',
        'imagen' :['assets\products\prin-collection1.txt'],
        'descripcion' : 'trio de anillos con piedras preciosas',
-       'stock_por_color': {'rosa': 22, 'morado': 53, 'azul': 55},
+       stock: [
+         { color: 'plata', talla: 6, cantidad: 22 },
+         { color: 'plata', talla: 7, cantidad: 15 },
+         { color: 'plata', talla: 8, cantidad: 32 },
+         { color: 'oro', talla: 6, cantidad: 21 },
+         { color: 'oro', talla: 7, cantidad: 21 },
+         { color: 'oro', talla: 8, cantidad: 19 }
+
+     ]
     },
    
     {
@@ -105,7 +158,15 @@ const productos = [
        'tipo_de_accesorio' : 'anillo',
        'imagen' :['assets\products\prin-collection2.txt'],
        'descripcion' : 'anilo con piedra camellian',
-       'stock_por_color': {'naranja': 54, 'amarillo': 53, 'dorado': 55},
+       stock: [
+         { color: 'plata', talla: 6, cantidad: 30 },
+         { color: 'plata', talla: 7, cantidad: 15 },
+         { color: 'plata', talla: 8, cantidad: 26 },
+         { color: 'oro', talla: 6, cantidad: 22 },
+         { color: 'oro', talla: 7, cantidad: 15 },
+         { color: 'oro', talla: 8, cantidad: 18 }
+
+     ]
     },
 
     {
@@ -115,8 +176,16 @@ const productos = [
       'precio_unitario' : '620.73',
       'tipo_de_accesorio' : 'anillo',
       'imagen' :['assets\products\prin-collection3.txt'],
-      'descripcion' : 'anillo con ojas de oro',
-      'stock_por_color': {'oro': 22, 'plata': 53},
+      'descripcion' : 'anillo en hojas',
+      stock: [
+         { color: 'plata', talla: 6, cantidad: 30 },
+         { color: 'plata', talla: 7, cantidad: 15 },
+         { color: 'plata', talla: 8, cantidad: 26 },
+         { color: 'oro', talla: 6, cantidad: 22 },
+         { color: 'oro', talla: 7, cantidad: 15 },
+         { color: 'oro', talla: 8, cantidad: 18 }
+
+     ]
    },
 
    {
@@ -127,7 +196,11 @@ const productos = [
       'tipo_de_accesorio' : 'anillo',
       'imagen' :['assets\products\prin-collection4.txt'],
       'descripcion' : 'anillo con micro cristales ',
-      'stock_por_color': {'plata': 22, 'oro': 53},
+      stock: [
+         { color: 'plata', talla: 6, cantidad: 54 },
+         { color: 'plata', talla: 7, cantidad: 43 },
+         { color: 'plata', talla: 8, cantidad: 20 }
+     ]
    },
 
    {
@@ -138,8 +211,59 @@ const productos = [
       'tipo_de_accesorio' : 'anillo',
       'imagen' :['assets\products\prin-collection5.txt'],
       'descripcion' : 'anillo delicado con murano ruso',
-      'stock_por_color': {'plata': 22, 'oro': 66},
-   },
+      stock: [
+         { color: 'plata', talla: 6, cantidad: 30 },
+         { color: 'plata', talla: 7, cantidad: 15 },
+         { color: 'plata', talla: 8, cantidad: 26 },
+         { color: 'oro', talla: 6, cantidad: 22 },
+         { color: 'oro', talla: 7, cantidad: 15 },
+         { color: 'oro', talla: 8, cantidad: 18 }
+
+     ]
+ },
     
     
 ];
+
+
+// • Crear una función que ordene un array de productos por precios de manera ascendente
+// y descendente y retorne el array resultante. Ejecutar la función y mostrar el resultado en
+// consola.
+
+const ordenarPorPrecio = (productos, ascendente = true) => {
+   const factorOrden = ascendente ? 1 : -1;
+   return productos.slice().sort((precioA, precioB) => (precioA.precio_unitario - precioB.precio_unitario) * factorOrden);
+};
+
+const productosOrdenadosAscendente = ordenarPorPrecio(productos, true);
+console.log("Productos ordenados por precio de manera ascendente:", productosOrdenadosAscendente);
+
+
+const productosOrdenadosDescendente = ordenarPorPrecio(productos, false);
+console.log("Productos ordenados por precio de manera descendente:", productosOrdenadosDescendente);
+
+// • Crear una función que calcule el total a pagar de una compra, reciba como parámetros
+// un array de productos donde cada producto, tenga como propiedades la cantidad y
+// precio unitario del producto y devuelva el valor que corresponda a la sumatoria de la
+// cantidad por el precio de cada producto. Ejecutar la función con datos de prueba y
+// mostrar el resultado en la consola del navegador.
+
+function calcularTotalCompra(productos) {
+   const total = productos.reduce((acumulador, producto) => {
+       return acumulador + (producto.cantidad * producto.precio_unitario);
+   }, 0);
+
+   return total;
+}
+const productosCompra = [
+   { cantidad: 2, precio_unitario: 620.73 },
+   { cantidad: 4, precio_unitario: 168.76 },
+   { cantidad: 5, precio_unitario: 327.71 }
+];
+
+
+const totalCompra = calcularTotalCompra(productosCompra);
+console.log("Total a pagar de la compra:", totalCompra)
+
+
+
