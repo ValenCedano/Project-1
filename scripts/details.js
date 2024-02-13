7fd6ed1de2c95d19a9734723d25fb1530ae1e882
Valentina Cedano <valentina.cedano57@gmail.com>
2024-02-13 11:14:35 -0500

    Cambios details

--- scripts/details.js
+++ scripts/details.js
@@ -10,7 +10,7 @@ const imagenPrincipal = document.querySelector('.imagen-principal');
 const imagenSecundaria = document.querySelector('.imagenes-secundarias'); // Utiliza querySelector para seleccionar por clase
 const nombreJoyeria = document.getElementsByClassName('menu-chiquito');
 const circulos = document.querySelector('.circles');
-
+const tallas = document.getElementsByClassName("Tallas");
 
 const titulo = document.getElementsByClassName('title');
 const code = document.getElementsByClassName('code');
@@ -68,34 +68,73 @@ const insertarSecundarias = (url) => {
 };
 
 const insertarCirculos = (objetocolor) => {
-    circulos.innerHTML = "";
+    circulos.innerHTML += "";
     const circulo = document.createElement('p');
     circulo.className = 'circle';
     circulo.style.backgroundColor = objetocolor;
     circulos.appendChild(circulo);
 };
 
+const text= document.querySelector('#numero');
+let contador = 0;
+text.innerHTML = contador;
 
+const sumar = () => {
+    contador++;
+    text.innerHTML= contador;
+}
+
+const restar = () => {
+    if(contador > 0){
+        contador--;
+        text.innerHTML= contador;
+    }
+}
 
 const cambiarDetalles = (contenedor,texto) => {
     contenedor[0].textContent = texto;
 }
+
+const guardarProductoAlCarrito = () => {
+    const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    const condicion1 = productoSeleccionado.tipoAccesorio == "anillo" && productoAComprar.cantidad && productoAComprar.color && productoAComprar.talla
+    const condicion2 = productoSeleccionado !== "anillo" && productoAComprar.cantidad && productoAComprar.color
+    if (condicion1 || condicion2) {
+      carrito.push(productoAComprar);
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+    } else {
+      alert("No se ha escogido un color, cantidad o talla")
+    }
+}
+  
+
 
 
 document.addEventListener("DOMContentLoaded", async () => {
     const url = `${URL_BASE}productos`;
     const productos = await getproducts(url);
     // Cambiando para el producto especifico //
+    
     const menuChiquito = cambiarNombre(productos[idProducto - 1].nombre);
     const tituloNuevo = cambiarDetalles(titulo, productos[idProducto - 1].nombre);
     const codeNuevo = cambiarDetalles(code, productos[idProducto - 1].codigo);
     const precioNuevo = cambiarDetalles(precio, productos[idProducto - 1].precio_unitario);
-    const stock = productos[idProducto - 1].stock
-    for(let i=0; i< stock.length; i++){
-        insertarCirculos(stock[i].color);
-    };
+    const stock = productos[idProducto - 1].stock;
     
-    // Cierre
+    let coloresInsertados = new Set();
+    
+    // Iterar sobre el objeto stock
+    for (let i = 0; i < stock.length; i++) {
+        console.log(stock[i].color);
+        // Verificar si el color ya ha sido insertado
+        if (!coloresInsertados.has(stock[i].color)) {
+            // Insertar el círculo del color si no se ha insertado antes
+            insertarCirculos(stock[i].color);
+            // Agregar el color al conjunto
+            coloresInsertados.add(stock[i].color);
+        }
+    }
+
     //Ingresando imagenes según el producto especificado
     const imagenesPequeñas = productos[idProducto - 1].imagen;
     const imgArray = pasarDeObjectArray(imagenesPequeñas);
@@ -115,6 +154,28 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     insertarPrincipal(imgArray[0]);
     //cierre
+   
+
+    for (let i = 0; i < stock.length; i++) {
+        // Verificar si el objeto en el stock tiene el atributo 'tallas'
+        if (stock[i].hasOwnProperty('talla')) {
+           return tallas;
+        } else {
+            return tallas[0].remove();
+        }
+    };
+
+
+
+    
+
+    
+   
+        
+    
+    //Terminacion
+
+
 
     //Detalles ciruclos
 
